60c517533e54cedd81cb8c7fddabdeab10bf0d01
sigmasigmasigmaboy <iseeyouman125@gmail.com>
2024-12-07 01:11:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+// Select the button by its ID
+const button = document.getElementById('myButton');
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    // Action to take when the button is clicked
+    alert('Button clicked!');
+});
